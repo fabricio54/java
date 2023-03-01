014c89d8616132dd333fd80f6f05903196e77abb
Fabricio Alves <fabricioallves2020@gmail.com>
2023-03-01 13:45:38 -0300

    debug erros

---
+++ br.com.java.excessoes/Debug.java
@@ -0,0 +1,14 @@
+public class Debug {
+    public static void main(String[] args) {
+        // debug                 debub avançado
+        // Breakpoints           Breakpoint properti
+        // Variables             Watchpoint
+        // Expressions           Exception Breakpoint
+        // Navegação             Class Breakpoint
+        // F5, F6, F7, F8 
+        
+        int resultado;
+
+        public
+    }
+}
